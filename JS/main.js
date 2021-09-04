ae0fa2febba2877f92bdffd09676192636f8dac2
Harshal Gami <77873685+harshalgami13@users.noreply.github.com>
2021-09-04 19:01:02 +0530

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -2,4 +2,13 @@ $(document).ready(function () {
     $('.navmenu').click(function () {
         $('.mobileview').slideToggle(500)
     });
+    function checkwidth() {
+        var windowSize = $(window).width();
+        if (windowSize >= 769) {
+            $('.mobileview').hide()
+        }
+    }
+    checkwidth();
+    // Bind event listener
+    $(window).resize(checkwidth);
 });
